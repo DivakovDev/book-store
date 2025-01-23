719212ec14f34a2c22418e7da3311a1ee0431ee8
Lyusi Divakov <divakovwork@gmail.com>
2025-01-23 12:20:54 +0200

    Decrease the scale width

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Navbar/>
-      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+      <main className='max-w-screen-xl min-h-screen px-4 py-6 mx-auto font-primary'>
         <Outlet />
       </main>
       <footer>Footer</footer>
