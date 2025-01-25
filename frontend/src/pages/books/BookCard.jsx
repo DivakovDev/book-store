8203244976de6d3da05c42e035458d7c0c706567
Lyusi Divakov <divakovwork@gmail.com>
2025-01-25 16:42:30 +0200

    Add redux logics for creating cart and adding products in it!

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -1,8 +1,15 @@
 import { FiShoppingCart } from "react-icons/fi";
 import { getImgUrl } from "../../utils/getImgUrl";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 
 export const BookCard = ({ book }) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (prod) => {
+    dispatch(addToCart(prod));
+  };
   return (
     <div className="transition-shadow duration-300 rounded-lg">
       <div className="flex flex-row gap-4 sm:flex-row sm:items-center sm:h-72 sm:justify-center">
@@ -16,20 +23,27 @@ export const BookCard = ({ book }) => {
           </Link>
         </div>
 
-        <div className='flex flex-col items-start justify-center'>
+        <div className="flex flex-col items-start justify-center">
           <Link to={`/books/${book._id}`}>
             <h3 className="h-full mb-3 overflow-hidden text-sm font-semibold sm:text-xl hover:text-blue-600 line-clamp-2 text-ellipsis">
               {book.title}
             </h3>
           </Link>
-          <p className="mb-5 text-xs text-gray-600 sm:text-sm">{book.description.length > 50 ? `${book.description.slice(0,50)}...` : book.description}</p>
+          <p className="mb-5 text-xs text-gray-600 sm:text-sm">
+            {book.description.length > 50
+              ? `${book.description.slice(0, 50)}...`
+              : book.description}
+          </p>
           <p className="mb-5 text-xs font-medium sm:text-sm">
             ${book.newPrice}{" "}
             <span className="ml-2 font-normal line-through">
               ${book.oldPrice}
             </span>
           </p>
-          <button className="flex items-center gap-1 px-4 py-2 space-x-1 text-sm font-bold transition-all duration-200 rounded-md cursor-pointer sm:text-base bg-primary font-secondary hover:bg-secondary hover:text-white">
+          <button
+            onClick={() => handleAddToCart(book)}
+            className="flex items-center gap-1 px-4 py-2 space-x-1 text-sm font-bold transition-all duration-200 rounded-md cursor-pointer sm:text-base bg-primary font-secondary hover:bg-secondary hover:text-white"
+          >
             <FiShoppingCart className="" />
             <span>Add to Cart</span>
           </button>
---
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
+
+const initialState = {
+  cartItems: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item._id === action.payload._id
+      );
+      if (!existingItem) {
+        state.cartItems.push(action.payload);
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Product Added to the Cart",
+            showConfirmButton: false,
+            timer: 1500
+          });
+      } else {
+        Swal.fire({
+            title: "Already Added to the Cart?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "OK!"
+          });
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
