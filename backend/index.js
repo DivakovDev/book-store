8075f64d77fefabeef1e6dc94d56aa800fc5fe16
Lyusi Divakov <divakovwork@gmail.com>
2025-01-27 12:03:04 +0200

    Connect mongodb with our backend, install dotenv for protection of db url
    there is changes to make in mongo db app

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+require('dotenv').config()
+
 const port = process.env.PORT || 5000;
 
-app.use("/", (req, res) => {
-  res.send("Book server is Running!");
-});
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    app.use("/", (req, res) => {
+        res.send("Book server is Running!");
+    });
+}
+
+// 49bpweRJ3vNyonGb
+
+main()
+  .then(() => console.log("MongoDb Connect Successfully!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log("Your app listen port:", port);
