7494657c9487e3bce851a89f6aefd7c03b55284b
Lyusi Divakov <divakovwork@gmail.com>
2025-01-29 16:27:52 +0200

    Create single book page for single books and add it to the router

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -5,6 +5,7 @@ import { Login } from "../components/Login";
 import { Register } from "../components/Register";
 import { CartPage } from "../pages/books/CartPage";
 import { CheckoutPage } from "../pages/books/CheckoutPage";
+import { SingleBook } from "../pages/books/SingleBook";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         { path: "/register", element: <Register/> },
         { path: "/cart", element: <CartPage/> },
         { path: "/checkout", element: <CheckoutPage/> },
+        { path: "/books/:id", element: <SingleBook/> },
     ],
   },
 ]);
