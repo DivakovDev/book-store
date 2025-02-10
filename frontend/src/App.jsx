4213fca6079878624fcdecd452e344b9059ce260
Lyusi Divakov <divakovwork@gmail.com>
2025-02-10 17:58:52 +0200

    Implement Auth logics in project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
+import { AuthProvider } from "../context/AuhtContext";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <main className="max-w-screen-xl min-h-screen px-4 mx-auto sm:py-6 2xl:max-w-screen-2xl font-primary">
-        <Outlet />
-      </main>
-      <Footer />
+      <AuthProvider>
+        <Navbar />
+        <main className="max-w-screen-xl min-h-screen px-4 mx-auto sm:py-6 2xl:max-w-screen-2xl font-primary">
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
