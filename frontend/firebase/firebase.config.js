e38a3360248dd184f451336e8de9544fd58cb60b
Lyusi Divakov <divakovwork@gmail.com>
2025-02-11 17:26:43 +0200

    Fix mistake which i made with import location

--- frontend/firebase/firebase.config.js
+++ frontend/firebase/firebase.config.js
@@ -1,6 +1,6 @@
-import { getAuth } from 'firebase/app';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +11,9 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
