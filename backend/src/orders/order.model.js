986dad2a157ad6566bcb5b8ce190e728db785068
Lyusi Divakov <divakovwork@gmail.com>
2025-02-15 20:38:54 +0200

    Create order controller, model and route.

---
+++ backend/src/orders/order.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    address: {
+      city: {
+        type: String,
+        require: true,
+      },
+      country: String,
+      state: String,
+      zipcode: String,
+    },
+    phone: {
+      type: Number,
+      require: true,
+    },
+    productsIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+        require: true,
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
