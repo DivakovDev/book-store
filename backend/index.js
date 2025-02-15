52211b5a5ac098472c99a4492d8b53ec72c9b099
Lyusi Divakov <divakovwork@gmail.com>
2025-02-15 20:39:55 +0200

    Create orders api and implement it to store.js and index.js files

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json())
-app.use(cors({
-    origin:['http://localhost:5173'],
-    credentials:true
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 // routes
-const bookRoutes = require('./src/books/book.route')
-app.use('/api/books',bookRoutes)
+const bookRoutes = require("./src/books/book.route");
+const orderRoutes = require("./src/orders/order.route");
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
 
 async function main() {
-    await mongoose.connect(process.env.DB_URL);
-    app.use("/", (req, res) => {
-        res.send("Book server is Running!");
-    });
+  await mongoose.connect(process.env.DB_URL);
+  app.use("/", (req, res) => {
+    res.send("Book server is Running!");
+  });
 }
 
 main()
