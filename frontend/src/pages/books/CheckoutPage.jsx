d3931b931188420ab0d4822d300409e64e96c05f
Lyusi Divakov <divakovwork@gmail.com>
2025-02-15 20:40:28 +0200

    Add functionality to checkout page but still something wrong with products info gathering

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -1,17 +1,20 @@
 import { useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useAuth } from "../../../context/AuthContext";
+import Swal from "sweetalert2";
+import { useCreateOrderMutation } from "../../redux/orders/ordersApi";
 
 export const CheckoutPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const navigate = useNavigate();
 
   const totalPrice = cartItems
     .reduce((acc, item) => acc + item.newPrice, 0)
     .toFixed(2);
 
-  const {currentUser} = useAuth();
+  const { currentUser } = useAuth();
   const {
     register,
     handleSubmit,
@@ -19,7 +22,9 @@ export const CheckoutPage = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
+  const [createOrder, { isLoading, error }] = useCreateOrderMutation();
+
+  const onSubmit = async (data) => {
     console.log(data);
     const newOrder = {
       name: data.name,
@@ -34,10 +39,27 @@ export const CheckoutPage = () => {
       productIds: cartItems.map((item) => item?._id),
       totalPrice: totalPrice,
     };
-    console.log(newOrder);
+
+    try {
+      await createOrder(newOrder).unwrap();
+      Swal.fire({
+        title: "Confirmed Order",
+        text: "Your order placed successfully!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, It's Okay!",
+      });
+      navigate("/orders");
+    } catch (error) {
+      console.error("Error place an order", error);
+      alert("Failed to place an order");
+    }
   };
   const [isChecked, setIsChecked] = useState(false);
 
+  if (isLoading) return <div>Loading....</div>;
   return (
     <section>
       <div className="flex items-center justify-center min-h-screen p-6 bg-gray-100">
