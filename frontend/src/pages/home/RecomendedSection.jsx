dee6825bca68cf958722644b0f8d8edfc6c7fc24
Lyusi Divakov <divakovwork@gmail.com>
2025-02-23 16:49:14 +0200

    Remove static book loading and implment books from database

--- frontend/src/pages/home/RecomendedSection.jsx
+++ frontend/src/pages/home/RecomendedSection.jsx
@@ -1,60 +1,53 @@
-import { useEffect, useState } from "react";
 import { BookCard } from "../books/BookCard";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useFetchAllBooksQuery } from "../../redux/features/books/booksApi";
+
 
 import "swiper/css";
 import "swiper/css/pagination";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 
-import { Pagination, Navigation} from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 
 export const RecomendedSection = () => {
-     const [books, setBooks] = useState([]);
-     
-      useEffect(() => {
-        fetch("books.json")
-          .then((res) => res.json())
-          .then((data) => setBooks(data));
-      }, []);
+  const { data: books = [] } = useFetchAllBooksQuery();
 
   return (
-    <div className='pt-10 pb-16'>
+    <div className="pt-10 pb-16">
       <h2 className="mb-6 text-3xl font-semibold">Recommended for you</h2>
       <div className="flex items-center">
-      <Swiper
-        navigation={true}
-        slidesPerView={1}
-        spaceBetween={30}
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 1,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 2,
-            spaceBetween: 50,
-          },
-          1180:{
-            slidesPerView: 3,
-            spaceBetween: 50,
-          }
-        }}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        {
-        books.slice(8,16).map((book, index) => (
-          <SwiperSlide key={index}>
-            <BookCard book={book} />
-          </SwiperSlide>
-        ))
-        }
-      </Swiper>
+        <Swiper
+          navigation={true}
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 1,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 50,
+            },
+            1180: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {books.slice(8, 16).map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
-  )
-}
+  );
+};
